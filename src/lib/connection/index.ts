8768d3a2db51084ee3534cb300f21e688e874891
Henrique Leite <henriqueleite616@gmail.com>
2021-10-09 08:00:48 -0300

    Update Template (#5)

--- src/lib/connection/index.ts
+++ src/lib/connection/index.ts
@@ -1,13 +1,39 @@
-import { Connection } from "@techmmunity/symbiosis";
+import { BaseConnectionOptions, Connection } from "@techmmunity/symbiosis";
 import { ExampleRepository } from "../repository";
 import { ColumnExtraMetadata } from "../types/column-extra-metadata";
 import { EntityExtraMetadata } from "../types/entity-extra-metadata";
+
+/**
+ * Example type:
+ * DynamoDBClientConfig
+ */
+export type LibToConnectToDbOptionsType = any;
 
 export class ExampleConnection extends Connection<
 	EntityExtraMetadata,
 	ColumnExtraMetadata
 > {
+	private readonly connectionInstance: LibToConnectToDbOptionsType;
+
+	public constructor(
+		options: BaseConnectionOptions<LibToConnectToDbOptionsType>,
+	) {
+		super(options);
+
+		this.connectionInstance = {};
+		/*
+		 * Example:
+		 * this.connectionInstance = new DynamoDBClient(
+		 * 	options.databaseConnectionConfig || {},
+		 * );
+		 */
+	}
+
 	public getRepository<Entity>(entity: Entity) {
-		return new ExampleRepository(entity);
+		return new ExampleRepository(
+			this.connectionInstance,
+			this.entityManager,
+			entity,
+		);
 	}
 }
